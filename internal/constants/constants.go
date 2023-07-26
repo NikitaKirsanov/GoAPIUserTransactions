ec79576933071877507b2eb9b3d004521f37a7e5
Nikita Kirsanov <nicas33@mail.ru>
2023-07-26 22:33:16 +0200

    fix

--- internal/constants/constants.go
+++ internal/constants/constants.go
@@ -3,9 +3,6 @@ package constants
 const (
 	AppName                = "stavkatv"
 	SchemaName             = "stavkatv"
-	MigrationsTableName    = SchemaName + ".migrations"
-	UsersTableName         = SchemaName + ".users"
-	TransactionsTableName  = SchemaName + ".transactions"
 	DBTypePostgres         = "POSTGRES"
 	DBTypeRedis            = "REDIS"
 	RedisUserPrefix        = "user_"
