f0a137de14d336c4e551248045e845989cfc47e7
Nikita Kirsanov <nicas33@mail.ru>
2023-07-27 09:37:21 +0200

    ping

--- server/rounter.go
+++ server/rounter.go
@@ -1,6 +1,7 @@
 package server
 
 import (
+	"encoding/json"
 	"fmt"
 	"net/http"
 
@@ -10,6 +11,12 @@ import (
 func NewServer(s *Service) {
 	r := mux.NewRouter()
 
+	r.HandleFunc(
+		"/ping",
+		func(w http.ResponseWriter, r *http.Request) {
+			w.Header().Set("Content-Type", "application/json")
+			json.NewEncoder(w).Encode("PONG")
+		}).Methods("GET")
 	r.HandleFunc("/users", s.GetUsers).Methods("GET")
 	r.HandleFunc("/transfer", s.MakeTransfer).Methods("POST")
 	fmt.Println("Server started")
